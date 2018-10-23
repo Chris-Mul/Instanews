a61b9a4a4f966780deb5d311f1c08cb7d8a9352c
Chris <boggum@hotmail.com>
2018-10-23 13:02:05 +0100

    Pulling info,ready for styling

--- js/script.js
+++ js/script.js
@@ -1,32 +1,91 @@
-var url = "https://api.nytimes.com/svc/topstories/v2/.json";
+
+/*
+var X;
+
+
+
+let url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 url += '?' + $.param({
   'api-key': "82f12f3e4189492f86578cb2d4e6add0"
 });
+alert('test');
 
-console.log("test");
-console.log($("select").val());
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(data) { 
+  var newsCounter = 0
+for ( var i=0;i < data.results.length;  i++){
 
-$("select").on("change", function () {
-  var url = "https://api.nytimes.com/svc/topstories/v2/" + $("select").val() + ".json";
-  url += '?' + $.param({
-    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-  });
+let abstract = data.results[i].abstract;
+let artUrl = data.results[i].url;
+let imgUrl = data.results[i].multimedia[4].url;
+if  (data.results[i].multimedia < 4){
+  continue;
+}
 
-  console.log(url);
-
-  console.log($("select").val());
-
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function (result) {
-    console.log(result);
-  }).fail(function (err) {
-    throw err;
-  });
+console.log(abstract);
+console.log(artUrl);
+console.log(imgUrl)
 
 
+$(".artcontainer").append('<a href=""><div><p>test</p></div>></a>');
+$('.article').append('<a href="'+ artUrl +'" class="link"><img src="' + imgUrl + '"class="img" /><h1 class="abstract">' + abstract + '</h1></a>');
+
+newsCounter ++ ;
+if (newsCounter >= 12){
+  break;
+}
+
+}
+
+
+}).fail(function(err) {
+  throw err;
 });
 
 
+*/
+
+$(function () {
+  $('select').on('change', function (e) {
+      const section = $(this).val();
+      $('.articles').empty();
+
+      if (section) {
+          $('.container').addClass('loaded');
+          const url = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=82f12f3e4189492f86578cb2d4e6add0`;
+          $.ajax({
+              url: url,
+              method: 'GET',
+          }).done(function (data) {
+              let i = 0;
+              let articleAdded = 0;
+              while (i < data.results.length && articleAdded < 12) {
+                  i++;
+                  let currentResult = data.results[i];
+                  if (currentResult.multimedia.length) {
+                      articleAdded++;
+                      let abstract = currentResult.abstract;
+                      let artUrl = currentResult.url;
+                      let imgUrl = currentResult.multimedia[4].url;
+                      $('.articles').append(`
+                          <div>
+                              <a href="${artUrl}">
+                                  <img src="${imgUrl}">
+                                  <p>${abstract}</p>
+                              </a>
+                          </div>`);
+                  }
+              }
+          }).fail(function (err) {
+              throw err;
+          });
+
+      } else {
+          $('.container').removeClass('loaded');
+      }
+
+  })
+});
 
