51fc570e9395a7db9364fe9bcdaefc22e5bb2c2d
Chris <boggum@hotmail.com>
2018-10-24 21:09:29 +0100

    footer color update

--- js/script.js
+++ js/script.js
@@ -1,51 +1,4 @@
 
-/*
-var X;
-
-
-
-let url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-});
-alert('test');
-
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(data) { 
-  var newsCounter = 0
-for ( var i=0;i < data.results.length;  i++){
-
-let abstract = data.results[i].abstract;
-let artUrl = data.results[i].url;
-let imgUrl = data.results[i].multimedia[4].url;
-if  (data.results[i].multimedia < 4){
-  continue;
-}
-
-console.log(abstract);
-console.log(artUrl);
-console.log(imgUrl)
-
-
-$(".artcontainer").append('<a href=""><div><p>test</p></div>></a>');
-$('.article').append('<a href="'+ artUrl +'" class="link"><img src="' + imgUrl + '"class="img" /><h1 class="abstract">' + abstract + '</h1></a>');
-
-newsCounter ++ ;
-if (newsCounter >= 12){
-  break;
-}
-
-}
-
-
-}).fail(function(err) {
-  throw err;
-});
-
-
-*/
 
 $(function () {
   $('select').on('change', function (e) {
@@ -69,8 +22,7 @@ $(function () {
                       let abstract = currentResult.abstract;
                       let artUrl = currentResult.url;
                       let imgUrl = currentResult.multimedia[4].url;
-                      $('.articles').append(`
-                          <div class="articleContainer">
+                      $('.articles').append(`<div class="articleContainer">
                               <a href="${artUrl}">
                                 
                                   <p>${abstract}</p>
@@ -79,6 +31,7 @@ $(function () {
                           $('.articleContainer').last().css("background-image", 'url('+ imgUrl +')');
                   }
               }
+              
           }).fail(function (err) {
               throw err;
           });
